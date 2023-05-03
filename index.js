02b8652f82e8e284b9f85d8fd649c536c76f384c
kflanagan36 <115050012+kflanagan36@users.noreply.github.com>
2023-05-02 22:39:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,105 +9,57 @@ const unirest = require('unirest')
 
 const apiKey = process.env.API_KEY;
 const apiSecret = process.env.API_SECRET;
-const openaiApiKey = process.env.OPENAI_API_KEY;
 const eventUrl = process.env.EVENT_URL
 
 app.use(bodyParser.json())
 
-//const privateKey = require('fs').readFileSync('private.key');
-
 app.get('/webhooks/answer', (request, response) => {
 
   const ncco = [{
-      action: 'talk',
-      text: 'Hi, describe an image that you want to generate'
-    },
-    {
-      eventMethod: 'POST',
-      action: 'input',
-      eventUrl: [
-        eventUrl],
-      type: [ "speech" ],
-      speech: {
-        language: 'en-gb',
-        endOnSilence: 0.5
-      }
-    },
-    {
-      action: 'talk',
-      text: 'Thank you'
-    }
-  ]
+    action: 'talk',
+    text: 'Hi, please send a message to receive a response'
+  }]
+  
   console.log('/webhooks/answer')
   response.json(ncco)
-  
 })
 
-app.post('/webhooks/asr', (request, response) => {
+app.post('/webhooks/sms', (request, response) => {
+  
+  console.log(request.body)
+  console.log(request.body.msisdn)
+  const phoneNumber = request.body.msisdn
+  console.log(request.body.text)
+  const receivedMsg = request.body.text
+  
+  // Do something with the received message and generate a response
+  const responseMsg = `Thank you for your message: "${receivedMsg}". We will get back to you soon.`
+  
+  sendMsg(phoneNumber, responseMsg)
 
-    console.log(request.body)
-    console.log(request.body.speech.results[0].text)
-    console.log(request.body.from)
-    let phoneNumber = request.body.from
-    console.log(request.body.timestamp)
-    let promptText = request.body.speech.results[0].text
-    
-    var req = unirest('POST', 'https://api.openai.com/v1/images/generations')
+  response.status(204).send()
+})
+
+function sendMsg(phoneNumber, responseMsg) { 
+  console.log('sendMsg');
+  console.log(phoneNumber, responseMsg);
+
+  var req = unirest('POST', 'https://messages-sandbox.nexmo.com/v1/messages')
     .headers({
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + openaiApiKey
+      'Authorization': 'Basic ' + Buffer.from(apiKey + ':' + apiSecret).toString('base64'),
+      'Content-Type': 'application/json'
     })
     .send(JSON.stringify({
-      "prompt": promptText,
-      "n": 1,
-      "size": "1024x1024"
+      "from": "NEXMO",
+      "to": phoneNumber,
+      "message_type": "text",
+      "text": responseMsg
     }))
     .end(function (res) { 
       if (res.error) throw new Error(res.error); 
       console.log(res.raw_body);
-      console.log(res.body.data[0].url)
-      let imgUrl = res.body.data[0].url
-      sentMsg(phoneNumber, imgUrl)
-
     });
-
-    const ncco = [{
-      action: 'talk',
-      text: `Got it, I'll sent link to generated image in WhatsApp`
-    }]
-    response.json(ncco)
-    
-  })
-
-function sentMsg(phoneNumber, imgUrl) { 
-  console.log('sentMsg');
-    console.log(phoneNumber, imgUrl);
-
-    var req = unirest('POST', 'https://messages-sandbox.nexmo.com/v1/messages')
-  .headers({
-    'Authorization': 'Basic ' + Buffer.from(apiKey + ':' + apiSecret).toString('base64'),
-    'Content-Type': 'application/json'
-  })
-  .send(JSON.stringify({
-    "from": "14157386102",
-    "to": phoneNumber,
-    "message_type": "text",
-    "text": imgUrl,
-    "channel": "whatsapp"
-  }))
-  .end(function (res) { 
-    if (res.error) throw new Error(res.error); 
-    console.log(res.raw_body);
-  });
 }
-
-
-app.post('/webhooks/events', (request, response) => {
-    
-    console.log('/webhooks/events')
-    console.log(request.body)
-    response.sendStatus(200);  
-  })
 
 const port = 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
